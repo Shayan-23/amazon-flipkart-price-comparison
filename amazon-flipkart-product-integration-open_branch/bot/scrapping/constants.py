2bef829b04fb805e1b97d70b4a267f277bc48aa3
Shayan Crasta <112373098+Shayan-23@users.noreply.github.com>
2023-07-08 14:58:28 +0530

    Add files via upload

---
+++ amazon-flipkart-product-integration-open_branch/bot/scrapping/constants.py
@@ -0,0 +1,4 @@
+# change driver path to the path where chromedriver is located in your machine.
+DRIVER_PATH = r";C:\Users\shivaprasad\Desktop\adpmini\adpmini\bot\scrapping"
+FLIPCART_URL = "https://flipkart.com"
+AMAZON_URL = "https://amazon.com"
